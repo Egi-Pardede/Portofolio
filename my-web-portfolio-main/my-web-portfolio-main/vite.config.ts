b79d21b66edcb87fb74a548b0002ac221c5b7984
Egi-Pardede <pardede281204@gmail.com>
2025-11-19 22:26:24 +0700

    .

--- my-web-portfolio-main/my-web-portfolio-main/vite.config.ts
+++ my-web-portfolio-main/my-web-portfolio-main/vite.config.ts
@@ -3,22 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Ganti sesuai nama repo kamu
-const repoName = "Portofolio";
-
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? `/${repoName}/` : "/",
-
+  base: mode === "production" ? "/Portofolio/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
